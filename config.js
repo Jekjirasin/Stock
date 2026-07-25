ce1be6426fa6a47f720455dfea32b3a172fe4396
Jekjirasin <87799912+Jekjirasin@users.noreply.github.com>
2026-07-25 15:40:25 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-// วาง Web app URL ของ Google Apps Script ที่ได้จากขั้นตอน Deploy ตรงนี้
-// ตัวอย่าง: https://script.google.com/macros/s/AKfycb.../exec
-const API_URL = 'https://script.google.com/macros/s/AKfycbylJbV8NiFpmz1k_XIAFucBF-0WhpHmD_Le-eG5whaIsMA9FhKkIfbC1SATHEXDbZkJ/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbxyYAizX8IAFcBqpJPHngs5IZcrKvU9_a-2SplMPKLtycAZEmgPrbmb0_0pBJmkf-ai/exec';
